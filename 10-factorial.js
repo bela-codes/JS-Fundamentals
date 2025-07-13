7737ad4f3e772f0aa2b9b555c15419e8a74c22a1
Abel Tesfaye <abel.tesfaye.codes@gmail.com>
2025-07-13 18:50:53 +0300

    'Advanced js'

---
+++ 10-factorial.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const input = Number(process.argv[2]);
+
+function factorial(n) {
+  if (Number.isNaN(n) || n === 0) {
+    return 1;
+  }
+  return n * factorial(n - 1);
+}
+
+console.log(factorial(input));
