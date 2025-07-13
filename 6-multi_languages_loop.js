dcc35317ee620c0332abdc219dbdaf9c91219196
Abel Tesfaye <abel.tesfaye.codes@gmail.com>
2025-07-13 16:23:17 +0300

    corrected

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,5 +1,11 @@
 const loveLanguage = ["C is fun", "Python is cool", "Javascript is amazing"];
 
-for(let i = 0; i < loveLanguage.length; i++){
+// for(let i = 0; i < loveLanguage.length; i++){
+//     console.log(loveLanguage[i]);
+// }
+
+let i = 0
+while(i < 3){
     console.log(loveLanguage[i]);
+    i++;
 }
